baa43e9e1716394c9649aa57856dfd2f73b34a0a
matiasop <matiasop1998@gmail.com>
2022-07-08 08:26:04 -0400

    feat(dark-mode): add dark mode toggler component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
+	import DarkModeToggler from "./components/DarkModeToggler.svelte";
 </script>
 
 <main>
 	<h1>Hello World</h1>
+	<DarkModeToggler />
 </main>
+
+<style>
+	:root {
+		--bg-color: #ffffff;
+		--text-color: #000000;
+	}
+
+	:global(body) {
+		background: var(--bg-color);
+		color: var(--text-color);
+	}
+
+	:global(body.dark-mode) {
+		--bg-color: #000000;
+		--text-color: #ffffff;
+	}
+</style>
---
+++ src/components/DarkModeToggler.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let darkMode: boolean = false;
+  function toggle() {
+    darkMode = !darkMode;
+    window.document.body.classList.toggle("dark-mode");
+  }
+</script>
+
+<button on:click={toggle}>
+  {#if darkMode}
+    Light Mode
+  {:else}
+    Dark Mode
+  {/if}
+</button>
