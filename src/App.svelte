a0e6ff9378990398958aed9d461dce2fce314312
matiasop <matiasop1998@gmail.com>
2022-07-08 17:01:40 -0400

    feat(fonts): add montserrat and garamond fonts

--- src/App.svelte
+++ src/App.svelte
@@ -11,15 +11,23 @@
 			<h1>Home</h1>
 		</Route>
 		<Route path="about">
-			<p>About</p>
+			<p class="body-text">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+				aliquam vitae id corrupti explicabo accusamus et amet tempora? Dicta,
+				doloremque a. Quos similique fugiat, deserunt sit voluptates reiciendis
+				quod impedit.
+			</p>
 		</Route>
 	</Router>
 </main>
 
 <style>
+	@import url("https://fonts.googleapis.com/css2?family=EB+Garamond&family=Montserrat:wght@600&display=swap");
 	:root {
 		--bg-color: white;
 		--text-color: #0f172a;
+		--header-font: "Montserrat";
+		--body-font: "EB Garamond";
 	}
 
 	:global(body) {
@@ -30,5 +38,14 @@
 	:global(body.dark-mode) {
 		--bg-color: #0f172a;
 		--text-color: #e2e8f0;
+	}
+
+	:global(.header-text) {
+		font-family: var(--header-font);
+	}
+
+	:global(.body-text) {
+		font-family: var(--body-font);
+		font-size: 1.5rem;
 	}
 </style>
