0e63a8c320b7e7927d6d5aa6d7bf46328ecca157
matiasop <matiasop1998@gmail.com>
2022-07-08 19:58:53 -0400

    feat(footer): add page footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,26 @@
+<footer id="footer">
+  <p>
+    Page made by <a class="link" href="https://github.com/matiasop">@matiasop</a
+    >
+  </p>
+</footer>
+
+<style>
+  #footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 4rem; /* Footer height */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-style: solid;
+    border-width: 1px 0 0 0;
+    border-color: #888;
+  }
+
+  .link {
+    /* text-decoration: none; */
+    color: var(--accent-color);
+  }
+</style>
