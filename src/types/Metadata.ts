440b9264c4921aded6ef797bce22fc1c94295f1b
matiasop <matiasop1998@gmail.com>
2022-09-05 22:32:14 -0400

    feat(types): add Metadata type

---
+++ src/types/Metadata.ts
@@ -0,0 +1,13 @@
+type Metadata = {
+  'author': string,
+  'book_author': string,
+  'isbn_10': string,
+  'isbn_13': string,
+  'language': string,
+  'page_count': number,
+  'published_date': string,
+  'small_thumbnail': string,
+  'thumbnail': string,
+}
+
+export default Metadata;
