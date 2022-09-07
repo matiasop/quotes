8181415add0920eb843655cf9a03ce2922e92e1a
matiasop <matiasop1998@gmail.com>
2022-09-07 00:33:44 -0400

    feat(books): add sortBy select

--- src/pages/Books.svelte
+++ src/pages/Books.svelte
@@ -3,18 +3,40 @@
 
   import booksMetadata from "../data/books_metadata.json";
   import getDataFromJSON from "../utils/getDataFromJSON";
-
-  import SearchBar from "../components/SearchBar.svelte";
+  import compareStrings from "../utils/compareStrings";
 
   import type Metadata from "../types/Metadata";
   import type BookList from "../types/BookList";
+  import SortOption from "../types/SortOption";
 
-  const books: BookList[] = getDataFromJSON<Metadata>(booksMetadata);
+  import SearchBar from "../components/SearchBar.svelte";
+
+  const sortBooks = (books: BookList[], sortBy: SortOption): BookList[] => {
+    if (sortBy === SortOption.Title) {
+      return books.sort((a, b) => compareStrings(a.title, b.title));
+    } else if (sortBy === SortOption.Author) {
+      return books.sort((a, b) => compareStrings(a.data.author, b.data.author));
+    } else if (sortBy === SortOption.Date) {
+      return books.sort((a, b) =>
+        compareStrings(a.data.published_date, b.data.published_date)
+      );
+    }
+    return books;
+  };
+
+  let sortBy: SortOption = SortOption.Title;
+  let books: BookList[];
+  $: books = sortBooks(getDataFromJSON<Metadata>(booksMetadata), sortBy);
   let filteredBooks: BookList[] = [];
 </script>
 
 <h1>Books</h1>
 <SearchBar allBooks={books} bind:filteredBooks />
+<select bind:value={sortBy}>
+  <option value={SortOption.Title}>Title</option>
+  <option value={SortOption.Author}>Author</option>
+  <option value={SortOption.Date}>Published Date</option>
+</select>
 
 {#each filteredBooks as book}
   <div>
